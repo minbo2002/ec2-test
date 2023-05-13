bd767e08702c978123468432f67d27913f021806
minbo2002 <minbo2002@naver.com>
2023-05-13 15:56:15 +0900

    첫번째 세팅

---
+++ src/main/java/com/study/sidechallenge/SideChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.study.sidechallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SideChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SideChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/sidechallenge/SideChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.sidechallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SideChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
